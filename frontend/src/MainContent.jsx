0f5f396f7466b57971acd5e1ab28b21dac4fefd0
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-03 13:51:23 -0800

    Update state of the components by using setstate method

--- frontend/src/MainContent.jsx
+++ frontend/src/MainContent.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 export default class MainContent extends Component {
-  state = { appTitle: "Customers", customerCount: "5" };
+  state = { appTitle: "Customers", customersCount: "5" };
   render() {
     return (
       <div>
         <h4 className="border-bottom m-1 p-1">
           {this.state.appTitle}
           <span className="badge badge-secondary m-2">
-            {this.state.customerCount}
+            {this.state.customersCount}
           </span>
           <button className="btn btn-info" onClick={this.onRefreshClick}>
             Refresh
@@ -19,7 +19,7 @@ export default class MainContent extends Component {
   }
 
   //Executes when the user clicks on Refresh button
-  onRefreshClick(){
-      console.log("Refresh clicked");
+  onRefreshClick = () =>{
+      this.setState({customersCount: 7,});
   }
 }
