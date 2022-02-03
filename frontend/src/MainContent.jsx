8cd9d12e36bed3f57b2e88620754012ab1689724
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-03 13:30:11 -0800

    Render a specific expression

--- frontend/src/MainContent.jsx
+++ frontend/src/MainContent.jsx
@@ -1,7 +1,12 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-export default class MainContent extends Component{
-    render(){
-        return <div>Hello from Main Content</div>;
-    }
+export default class MainContent extends Component {
+  state = { appTitle: "Customers" };
+  render() {
+    return (
+      <div>
+        <h4 className="border-bottom m-1 p-1">{this.state.appTitle}</h4>
+      </div>
+    );
+  }
 }
