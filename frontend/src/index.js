209a2d706bad7d73e0399995e88ebe2e4916cc3a
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-03 11:23:57 -0800

    Created my first react app
    This required me to activate it on Visual Code terminal, not the Computer's terminal which was more complex.

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var element = <div>Hello World</div>;
+ReactDOM.render(element, document.getElementById("root"));
+
+
+console.log(element);
