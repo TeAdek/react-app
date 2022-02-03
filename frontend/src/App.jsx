73b98c2571c7f18f80e9f90bd4200f2607383f9e
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-03 12:36:46 -0800

    Add Bootstrap navbar
    !important overrides other css styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,37 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 class NavBar extends Component {
-    render(){
-        return(
-            <React.Fragment>
-                <h1>Hello</h1>
-                <p class="highlight">paragraph</p>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
+          <a class="navbar-brand" href="#">
+            My App
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </React.Fragment>
+    );
+  }
 }
 
 export default NavBar;
