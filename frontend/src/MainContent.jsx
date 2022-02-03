9732c86da81c62bb339964bb415b9d6843a6f964
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-03 13:43:50 -0800

    Handle events by clicks, drag and so on

--- frontend/src/MainContent.jsx
+++ frontend/src/MainContent.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 
 export default class MainContent extends Component {
-  state = { appTitle: "Customers" };
+  state = { appTitle: "Customers", customerCount: "5" };
   render() {
     return (
       <div>
-        <h4 className="border-bottom m-1 p-1">{this.state.appTitle}</h4>
+        <h4 className="border-bottom m-1 p-1">
+          {this.state.appTitle}
+          <span className="badge badge-secondary m-2">
+            {this.state.customerCount}
+          </span>
+          <button className="btn btn-info" onClick={this.onRefreshClick}>
+            Refresh
+          </button>
+        </h4>
       </div>
     );
+  }
+
+  //Executes when the user clicks on Refresh button
+  onRefreshClick(){
+      console.log("Refresh clicked");
   }
 }
