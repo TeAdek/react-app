0b38a8fd04a5f20a90e2127f30e7f605faa9b178
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-03 12:11:43 -0800

    Load Bootstrap and Create React components
    You can use div or React.Fragment (better choice) to return multiple elements. React.Fragment acts as parent or container for multiple extra html tags without adding extra div tag.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./App";
 
-var element = <div>Hello World</div>;
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<NavBar/>, document.getElementById("root"));
 
-
-console.log(element);
