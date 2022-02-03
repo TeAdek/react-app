b88e506ec84b66415885dcaec0569e4dbc730734
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-03 12:20:31 -0800

    Add CSS style to React
    You can also import it in .JSX

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./App";
+import "./index.css";
 
 ReactDOM.render(<NavBar/>, document.getElementById("root"));
 
