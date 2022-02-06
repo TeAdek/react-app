adb374a00849bdcbfe9913406ba6ee68a0ae3f8b
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-05 20:31:01 -0800

    Use OnChange event to create two-way binding in react manually
    This update the state values from the form

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -14,17 +14,30 @@ render(){
         {/**Email starts */}
         <div className="form-group form-row">
             <label className="col-lg-4">Email:</label>
-            <input type="text" className="form-control" value={this.state.email}/>
+            <input type="text" className="form-control" value={this.state.email}
+            onChange={(event)=>{this.setState({email: event.target.value});}}/>
         </div>
         {/**Email ends*/}
 
         {/**Password starts */}
         <div className="form-group form-row">
             <label className="col-lg-4">Password:</label>
-            <input type="password" className="form-control" value={this.state.password} />
+            <input type="password" className="form-control" value={this.state.password} 
+            onChange={(event)=>{this.setState({password: event.target.value});}}/>
         </div>
         {/**Password ends */}
+
+        <div>
+            <button className="btn btn-primary" onClick={this.onLoginClick}>
+                Login
+            </button>
         </div>
-    )
-}
+        </div>
+    );
+}//end of render
+
+//Executes when the user clicks on Login
+onLoginClick=() => {
+    console.log(this.state);
+};
 }
