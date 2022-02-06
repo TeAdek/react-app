e0d4c5d197cf5c472d7962ab2f5e64035e652d2a
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-05 18:05:54 -0800

    Create delete icon and delete the child components(products)
    Functions are handled in the parent and accessed through prop

--- frontend/src/Product.jsx
+++ frontend/src/Product.jsx
@@ -1,41 +1,63 @@
 import React, { Component } from "react";
 
 export default class Product extends Component {
-    state = {
-        product:this.props.product
-    }
+  state = {
+    product: this.props.product,
+  };
 
   render() {
     return (
       <div className="col-lg-6">
         <div className="card m-2">
           <div className="card-body">
-            <div className="text-muted">#{this.state.product.id}</div>
-            <h5 className="pt-2 border-top">{this.state.product.productName}</h5>
+            <div className="text-muted">
+              #{this.state.product.id}
+              {/* delete button */}
+              <span
+                className="pull-right hand-icon"
+                onClick={() => {
+                  this.props.onDelete(this.state.product);
+                }}
+              >
+                <i className="fa fa-times "></i>
+              </span>
+              {/** delete button ends */}
+            </div>
+            
+            <h5 className="pt-2 border-top">
+              {this.state.product.productName}
+            </h5>
             <div>${this.state.product.price}</div>
           </div>
           {/* card body ends here */}
 
           <div className="card-footer">
-              <div className="float-left">
-                  <span className="badge">{this.state.product.quantity}</span>
-                  <div className="btn-group">
-                      <button className="btn btn-outline-success"
-                      onClick={() => {this.props.onIncrement(this.state.product, 10)}}>
-                          +
-                      </button>
-                      <button className="btn btn-outline-success"
-                      onClick={() => {this.props.onDecrement(this.state.product, 0)}}>
-                          -
-                      </button>
-                  </div>
+            <div className="float-left">
+              <span className="badge">{this.state.product.quantity}</span>
+              <div className="btn-group">
+                <button
+                  className="btn btn-outline-success"
+                  onClick={() => {
+                    this.props.onIncrement(this.state.product, 10);
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  className="btn btn-outline-success"
+                  onClick={() => {
+                    this.props.onDecrement(this.state.product, 0);
+                  }}
+                >
+                  -
+                </button>
               </div>
-              {/* float-left ends here */}
+            </div>
+            {/* float-left ends here */}
 
-              <div className="float-right">{this.props.children}</div>
-              </div>
-              {/*card-footer ends here*/}
-              
+            <div className="float-right">{this.props.children}</div>
+          </div>
+          {/*card-footer ends here*/}
         </div>
       </div>
     );
