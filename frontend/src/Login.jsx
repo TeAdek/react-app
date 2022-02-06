50d290fa80ad044b57b92837be404c919cc1f271
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-05 20:54:28 -0800

    Use form button to validate user input

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,43 +1,72 @@
-import react, {Component} from "react";
+import react, { Component } from "react";
 
-export default class Login extends Component{
-constructor(props){
+export default class Login extends Component {
+  constructor(props) {
     super(props);
-    this.state = {email:"abc@test.com", password:"abc123"};
-}
+    this.state = { email: "", password: "", message: ""};
+  }
 
-render(){
-    return(
-        <div className="col-lg-9">
-            <h4 className="m-1 p-2 border-bottom">Login</h4>
-        
+  render() {
+    return (
+      <div className="col-lg-9">
+        <h4 className="m-1 p-2 border-bottom">Login</h4>
+
         {/**Email starts */}
         <div className="form-group form-row">
-            <label className="col-lg-4">Email:</label>
-            <input type="text" className="form-control" value={this.state.email}
-            onChange={(event)=>{this.setState({email: event.target.value});}}/>
+          <label className="col-lg-4">Email:</label>
+          <input
+            type="text"
+            className="form-control"
+            value={this.state.email}
+            onChange={(event) => {
+              this.setState({ email: event.target.value });
+            }}
+          />
         </div>
         {/**Email ends*/}
 
         {/**Password starts */}
         <div className="form-group form-row">
-            <label className="col-lg-4">Password:</label>
-            <input type="password" className="form-control" value={this.state.password} 
-            onChange={(event)=>{this.setState({password: event.target.value});}}/>
+          <label className="col-lg-4">Password:</label>
+          <input
+            type="password"
+            className="form-control"
+            value={this.state.password}
+            onChange={(event) => {
+              this.setState({ password: event.target.value });
+            }}
+          />
         </div>
         {/**Password ends */}
 
-        <div>
-            <button className="btn btn-primary" onClick={this.onLoginClick}>
-                Login
-            </button>
+        <div className="text-right">
+          {this.state.message}
+          <button className="btn btn-primary m-1" onClick={this.onLoginClick}>
+            Login
+          </button>
         </div>
-        </div>
+      </div>
     );
-}//end of render
+  } //end of render
 
-//Executes when the user clicks on Login
-onLoginClick=() => {
+  //Executes when the user clicks on Login
+  onLoginClick = () => {
     console.log(this.state);
-};
+    if (
+      this.state.email === "admin@test.com" &&
+      this.state.password === "admin123"
+    ) {
+      //success
+      this.setState({
+        message: <span className="text-success">Successfully Logged-in</span>,
+      });
+    } else {
+      //error
+      this.setState({
+        message: (
+          <span className="text-danger">Invalid login, please try again</span>
+        ),
+      });
+    }
+  };
 }
