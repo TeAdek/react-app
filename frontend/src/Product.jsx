17dfa263834626c12ede6765b9dce5a0977c008b
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-05 19:24:17 -0800

    Life cycle of child components

--- frontend/src/Product.jsx
+++ frontend/src/Product.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
 export default class Product extends Component {
-  state = {
-    product: this.props.product,
-  };
+
+constructor(props){
+    super(props);
+
+    console.log("Constructor - Product")
+
+    this.state = {
+        product: this.props.product,
+      };
+}
+  
 
   render() {
+
+    console.log("Render - Product")
+
     return (
       <div className="col-lg-6">
         <div className="card m-2">
@@ -61,6 +72,14 @@ export default class Product extends Component {
         </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount - Product");
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    console.log("componentDidUpdate - Product");
   }
 
   //Executes when the current instance of current component is being deleted from memory
